62a8439c4c6babfa8a8814cfb710415b780581f9
SergeyLebidko <it.sergeyler@mail.ru>
2020-03-14 20:07:47 +0300

    Add template for start page

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
     """Контроллер главной страницы"""
-    return HttpResponse('Главная страница')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта'})
