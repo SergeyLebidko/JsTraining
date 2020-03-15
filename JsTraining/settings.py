888f03f6ab193ce693a4e426701274a6e6788aa3
SergeyLebidko <it.sergeyler@mail.ru>
2020-03-15 18:55:51 +0300

    Add debug toolbar to project

--- JsTraining/settings.py
+++ JsTraining/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'debug_toolbar',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
 
 ROOT_URLCONF = 'JsTraining.urls'
 
