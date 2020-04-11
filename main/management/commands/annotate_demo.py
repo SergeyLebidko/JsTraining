65008291dceacdb23799441dc4a907b04e9c0e36
SergeyLebidko <it.sergeyler@mail.ru>
2020-04-11 19:07:59 +0300

    Add Case class and js func

--- main/management/commands/annotate_demo.py
+++ main/management/commands/annotate_demo.py
@@ -1,15 +1,24 @@
 from django.core.management.base import BaseCommand
 from main.models import Client, Order, Product
-from django.db.models import Count
+from django.db.models import Sum, Case, When, Value, CharField
 from django.db import connection
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        # orders = Order.objects.values(
+        #     'product__title'
+        # ).order_by('product__title').annotate(cnt=Count('product'))
+
         orders = Order.objects.values(
             'product__title'
-        ).order_by('product__title').annotate(cnt=Count('product'))
+        ).order_by(
+            'product'
+        ).annotate(
+            cnt=Sum('count'),
+            description=Case(When(cnt__gt=20, then=Value('Много')), default=Value('Мало'), output_field=CharField())
+        )
 
         print('\nРезультат запроса:')
         for order in orders:
--- main/static/main/hello.js
+++ main/static/main/hello.js
@@ -1,1 +1,10 @@
-document.write('<h3>Привет, из JS!</h3>')
+var txt = 'Глобальная переменная';
+
+document.write('<br>' + txt + '<br>');
+show();
+document.write(txt + '<br>');
+
+function show() {
+    txt = 'Локальная переменная';
+    document.write(txt + '<br>');
+}
