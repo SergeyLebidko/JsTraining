f05c24a99fc8422399c2fb48a94438ec65bf3167
SergeyLebidko <it.sergeyler@mail.ru>
2020-04-12 13:34:19 +0300

    Fix annotate demo command

--- main/management/commands/annotate_demo.py
+++ main/management/commands/annotate_demo.py
@@ -1,29 +1,60 @@
 from django.core.management.base import BaseCommand
 from main.models import Client, Order, Product
-from django.db.models import Sum, Case, When, Value, CharField
-from django.db import connection
+from django.db.models import Count, Sum, Case, When, Value, Q, CharField
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # orders = Order.objects.values(
-        #     'product__title'
-        # ).order_by('product__title').annotate(cnt=Count('product'))
+        """Команда - пример использования annotate в django"""
 
+        # Подсчитаем и выведем количество заказов, сделанных каждым клиентом
+        clients = Client.objects.values('title').order_by('title').annotate(orders_count=Count('order'))
+
+        print('\nКоличество заказов, сделанных каждым клиентом')
+        for client in clients:
+            print(f'{str(client["title"]).rjust(20, " ")} - {client["orders_count"]}')
+
+        # Подсчитаем и выведем количество заказанных товаров
+        products = Product.objects.values('title').order_by('title').annotate(products_count=Sum('order__count'))
+
+        print('\nКоличество заказанных единиц каждого товара')
+        for product in products:
+            print(f'{str(product["title"]).rjust(20, " ")} - {product["products_count"]}')
+
+        # Сводная информация по заказам
         orders = Order.objects.values(
+            'client__title',
             'product__title'
         ).order_by(
-            'product'
-        ).annotate(
-            cnt=Sum('count'),
-            description=Case(When(cnt__gt=20, then=Value('Много')), default=Value('Мало'), output_field=CharField())
+            'client__title',
+            'product__title'
+        ).annotate(total_count=Sum('count'))
+
+        print('\nСводная информация по озаказам')
+        for order in orders:
+            client_title = str(order["client__title"]).ljust(20, " ")
+            order_title = str(order["product__title"]).ljust(20, " ")
+            total_count = order["total_count"]
+            print(f'{client_title} - {order_title} - {total_count}')
+
+        # Счиает количество vip- и не vip-клиентов
+        clients = Client.objects.values('vip').order_by('vip').annotate(count=Count('id'))
+
+        print('\nКоличество vip- и не vip-клиентов')
+        for client in clients:
+            print(f'{"VIP-клиентов: " if client["vip"] else "Не VIP-клиентов: "}{client["count"]}')
+
+        # Использование annotate без функций для вывода кредитного лимита клиентов
+        clients = Client.objects.all().annotate(
+            description=Case(
+                When(credit_limit__gt=5000, then=Value('Большой')),
+                When(Q(credit_limit__gt=2000) & Q(credit_limit__lte=5000), then=Value('Средний')),
+                default=Value('Маленький'),
+                output_field=CharField()
+            )
         )
 
-        print('\nРезультат запроса:')
-        for order in orders:
-            print(order)
-
-        print('\nSQL:')
-        for sql in connection.queries:
-            print(sql)
+        print('\nКредитные лимиты клиентов')
+        for client in clients:
+            print(f'{str(client).ljust(20, " ")} - {client.description}')
--- main/management/commands/show_base.py
+++ main/management/commands/show_base.py
@@ -11,7 +11,9 @@ class Command(BaseCommand):
 
         print(f'\n{"-" * 15} Клиенты {"-" * 15}')
         for number, client in enumerate(clients, 1):
-            print(f'{number}. {client}')
+            title = str(client).ljust(20, " ")
+            credit_limit = str(client.credit_limit).ljust(10, " ")
+            print(f'{number}. {title} - {credit_limit} - {"vip" if client.vip else "" }')
 
         print(f'\n{"-" * 15} Товары {"-" * 15}')
         for number, product in enumerate(products, 1):
