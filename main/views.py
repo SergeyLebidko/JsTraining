47cb5b15d9e65593bb1c4afcb54e2b415a5e3e4f
SergeyLebidko <it.sergeyler@mail.ru>
2020-04-25 20:36:06 +0300

    Добалено отображение главных таблиц на главной странице

--- main/views.py
+++ main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Client, Order, Product
 
 
 def index(request):
     """Контроллер главной страницы"""
-    return render(request, 'main/index.html', {'title': 'Главная страница сайта'})
+
+    clients = Client.objects.all()
+    products = Product.objects.all()
+    orders = Order.objects.all()
+
+    context = {
+        'clients': clients,
+        'products': products,
+        'orders': orders
+    }
+    return render(request, 'main/index.html', context=context)
 
