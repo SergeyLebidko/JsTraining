391c16c35b7d0516004fa7f8105cbbbd494c8f7a
SergeyLebidko <it.sergeyler@mail.ru>
2020-04-26 16:37:33 +0300

    Добавлены кнопки в меню

--- main/static/main/hello.js
+++
@@ -1,8 +0,0 @@
-function Person(name, age, gender) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-}
-
-a = new Person('Сергей', 35, 'male');
-b = new Person('Юлия', 33, 'female');
